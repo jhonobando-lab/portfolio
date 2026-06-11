d30b2c07393e581f4e4390c6976bfcaa01d35a45
jhonobando-lab <jhonobando@gmail.com>
2026-06-11 10:00:00 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
-
 const PORT = process.env.PORT || 3000;
 
+const routes = {
+  '/': 'index.html',
+  '/visa': 'visa.html',
+  '/visa.html': 'visa.html',
+  '/iris': 'iris.html',
+  '/iris.html': 'iris.html',
+};
+
 http.createServer((req, res) => {
-  let file = 'index.html';
-  if (req.url === '/visa' || req.url === '/visa.html') file = 'visa.html';
-  
+  const file = routes[req.url] || 'index.html';
   fs.readFile(path.join(__dirname, file), (err, data) => {
     if (err) { res.writeHead(404); res.end('Not found'); return; }
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
