7aa37560d20a63f765a2401c95b362acb1f34b14
jhonobando-lab <jhonobando@gmail.com>
2026-06-11 08:51:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,11 @@ const path = require('path');
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-  fs.readFile(path.join(__dirname, 'index.html'), (err, data) => {
-    if (err) { res.writeHead(500); res.end('Error'); return; }
+  let file = 'index.html';
+  if (req.url === '/visa' || req.url === '/visa.html') file = 'visa.html';
+  
+  fs.readFile(path.join(__dirname, file), (err, data) => {
+    if (err) { res.writeHead(404); res.end('Not found'); return; }
     res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
     res.end(data);
   });
